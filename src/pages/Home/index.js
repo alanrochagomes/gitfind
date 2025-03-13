ea36f0a773083546cfa5f2497e3868c5b1fc7762
Alan Rocha <alanrochaarg2001@gmail.com>
2025-03-13 11:39:14 -0300

    Implementando imagem, input e botão

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,21 @@
 import { Header } from "../../components/Header/index";
+import background from "../../assets/background.svg";
+
+import "../Home/styles.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="background " />
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username" />
+            <button>Buscar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
